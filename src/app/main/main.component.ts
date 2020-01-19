42db4e9ebd7df5bf1fcfed5d62c32e81487957c9
tesla <kela256@gmail.com>
2020-01-19 11:08:42 +0200

    add method setTimeout and change name

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,18 +1,39 @@
-import { Component, DoCheck } from "@angular/core";
+import { Component, DoCheck,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+ } from "@angular/core";
 
 @Component({
   selector: "app-main",
   templateUrl: "./main.component.html",
-  styleUrls: ["./main.component.css"]
+  styleUrls: ["./main.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainComponent implements DoCheck {
   public user = new User();
 
-  constructor() {}
+  constructor(private cd: ChangeDetectorRef) {
+  }
 
   ngDoCheck(): void {
     console.log(" MainComponent");
   }
+
+
+  public changeNameBySetTimeout(): void {
+    setTimeout( () => {
+      console.log("setTimeout name: " + (this.user.name += "+"));
+    }, 1000 );
+  }
+
+  public changeNameBySetTimeoutWithChangeDetectorRef(): void {
+    setTimeout( () => {
+      console.log("setTimeout name with ChangeDetectorRef: " + 
+        (this.user.name += "+"));
+      this.cd.markForCheck();
+    }, 1000 );
+  }
+
 
   public changeName(): void {
     this.user.name += "+";
@@ -25,7 +46,7 @@ export class MainComponent implements DoCheck {
     this.user.surname += "=";
 
     console.log("");
-    console.log("surname: " + this.user.surname);
+    console.log("surname: " + this.user.surname);      
   }
 
 
@@ -36,7 +57,7 @@ export class MainComponent implements DoCheck {
     };
 
     console.log(""); 
-    console.log("name: " + this.user.name);
+    console.log("new object with name: " + this.user.name);
   }
 
   public createNewUserAndChangeSurname(): void {
@@ -46,7 +67,7 @@ export class MainComponent implements DoCheck {
     };
 
     console.log("");
-    console.log("surname: " + this.user.surname);
+    console.log("new object with  surname: " + this.user.surname);
   }
 }
 
